d457b2cc84e912c11bc2a68549c1d214b0af70de
Console <tiktok_sd@163.com>
2025-06-28 22:54:05 +0800

    feat header新增默认值为空字符串 修改文档

--- message/client/leftCornerNotify.py
+++ message/client/leftCornerNotify.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from ...constant.clientConstant import *
 
-def SendLocalMessage(pid, msg, header, color=ENUM.ColorCode.WHITE):
+
+def SendLocalMessage(msg, header='', color=ENUM.ColorCode.WHITE):
     """
-    给指定玩家发送消息
+    客户端发送一条消息
     (原SetClientMessage)
     :param header: 消息头 会被添加在msg的前面
     :param pid: 接收消息的玩家ID
@@ -11,4 +12,4 @@ def SendLocalMessage(pid, msg, header, color=ENUM.ColorCode.WHITE):
     :param color: 颜色 默认白色
     :return: 无
     """
-    TextNotifyComp.SetLeftCornerNotify(color+header+str(msg))
+    TextNotifyComp.SetLeftCornerNotify(color + header + str(msg))
