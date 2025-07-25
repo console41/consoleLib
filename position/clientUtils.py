1e8f997f4ea0184ed94e7628d67a9f5c1bd192c8
Console <tiktok_sd@163.com>
2025-07-25 17:34:44 +0800

    bugfix: 修复clientUtils和serverUtils中引用GetPosFromPlayerRot错误

--- position/clientUtils.py
+++ position/clientUtils.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-from .client.getPos import GetDistanceToSecondTargetPointFromPlayer
+from .client.getPos import GetPosFromPlayerRot
 from .client.getEntities import GetNearestEntity, GetNearestPlayer
