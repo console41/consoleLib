d0c5847b874a62b17b18b80d3e52ad9f9c28b283
Console <tiktok_sd@163.com>
2025-11-15 18:04:40 +0800

    feat: 新增AddRepeatedTimer AddTimer

--- time/clientUtils.py
+++ time/clientUtils.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from .client.gameTime import GetDay, GetTimeOfDay
+from .client.timer import AddTimer, AddRepeatedTimer
--- time/serverUtils.py
+++ time/serverUtils.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-
 from .server.gameTime import GetDay, GetTimeOfDay
+from .server.timer import AddTimer, AddRepeatedTimer
