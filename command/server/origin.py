c914c147935c0dbc2fc76fd870efc39facad7daf
Console <tiktok_sd@163.com>
2025-12-27 21:07:57 +0800

    refactor: 规范IsRunByPlayer函数 将has_key方法替换为in关键字 docs: 修改文档

--- command/server/origin.py
+++ command/server/origin.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-def IsRunByPlayer(originArgs):
+def IsRunByPlayer(args):
     """
     判断自定义指令是否由玩家运行
-    :param originArgs: CustomCommandTriggerServerEvent事件触发后的原始数据
+    :param args: CustomCommandTriggerServerEvent事件触发后的原始数据
     :return: 是否由玩家运行
     :rtype: bool
     """
-    return originArgs['origin'].has_key('entityId')
+    return 'entityId' in args['origin']
